daa5b8eccfb9ea72469113093ded8dd4d6309d5d
anclark686 <43712089+anclark686@users.noreply.github.com>
2022-08-13 03:31:29 -0400

    with zip, not fully working

--- src/App.js
+++ src/App.js
@@ -5,14 +5,39 @@ import { Form, Card, Button, Col, Row } from 'react-bootstrap'
 
 
 function App() {
-
+  const [zip, setZip] = useState("")
+  let lon = ""
+  let lat = ""
   const [date, setDate] = useState(new Date())
+  const [msg, setMsg] = useState("")
 
+  const getLonLat = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=${process.env.REACT_APP_GOOGLE_API}`)
+      const data = await response.json()
+      lat = data.results[0].geometry.location.lat
+      lon = data.results[0].geometry.location.lng
+      console.log(`longitude: ${lon}, latitude: ${lat}`)
+      console.log(typeof lon.toString())
+      setMsg("")
+      getWeather()
+    } catch (err) {
+      console.log(err)
+      setMsg("Location not found, please try again")
+    }
+  }
+
+  const getWeather = async () => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API}`)
+    const data = await response.json()
+    console.log(data)
+  }
 
   const { data, isLoading, errorMessage } = useOpenWeather({
     key: process.env.REACT_APP_WEATHER_API,
-    lat: '37',
-    lon: '-87',
+    lat: lat.toString(),
+    lon: lon.toString(),
     lang: 'en',
     unit: 'imperial'
   })
@@ -26,7 +51,27 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Card>
+        <p>{msg}</p>
+        <Form>
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="2">Zip Code or City, State</Form.Label>
+            <Col sm="10">
+              <Form.Control 
+                type="text" 
+                placeholder="12345" 
+                onChange={(e) => setZip(e.target.value)}/>
+            </Col>
+            <Button 
+              variant="light" 
+              type="submit"
+              onClick={getLonLat}>
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+        <p> Date : {date.toLocaleDateString()} | Time: {date.toLocaleTimeString()}</p>
+      </Card>
       <ReactWeather
         isLoading={isLoading}
         errorMessage={errorMessage}
