265e66b09f3e14a7f6a01f53a6bbcc2cb19e9d28
anclark686 <43712089+anclark686@users.noreply.github.com>
2022-08-14 15:19:50 -0400

    finished?

--- public/electron.js
+++ public/electron.js
@@ -7,7 +7,7 @@ function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 640,
     webPreferences: {
       nodeIntegration: true,
     },
--- src/App.js
+++ src/App.js
@@ -4,43 +4,55 @@ import React, { useState, useEffect } from 'react'
 import { Form, Card, Button, Col, Row } from 'react-bootstrap'
 
 
+const GetWeather = (props) => {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: process.env.REACT_APP_WEATHER_API,
+    lat: props.lat,
+    lon: props.lon,
+    lang: 'en',
+    unit: 'imperial'
+  })
+
+  
+
+  return (
+    <ReactWeather
+      isLoading={isLoading}
+      errorMessage={errorMessage}
+      data={data}
+      lang="en"
+      locationLabel={props.city}
+      unitsLabels={{ temperature: 'F', windSpeed: 'mph' }}
+      showForecast
+    />
+  )
+}
+
+
 function App() {
   const [zip, setZip] = useState("")
-  let lon = ""
-  let lat = ""
+  const [lat, setLat] = useState("")
+  const [lon, setLon] = useState("")
   const [date, setDate] = useState(new Date())
   const [msg, setMsg] = useState("")
+  const [city, setCity] = useState("")
 
   const getLonLat = async (e) => {
     e.preventDefault()
     try {
       const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=${process.env.REACT_APP_GOOGLE_API}`)
       const data = await response.json()
-      lat = data.results[0].geometry.location.lat
-      lon = data.results[0].geometry.location.lng
+      setCity(data.results[0].address_components[1].long_name)
+      console.log(data.results[0])
+      setLat(data.results[0].geometry.location.lat.toString())
+      setLon(data.results[0].geometry.location.lng.toString())
       console.log(`longitude: ${lon}, latitude: ${lat}`)
-      console.log(typeof lon.toString())
       setMsg("")
-      getWeather()
     } catch (err) {
       console.log(err)
       setMsg("Location not found, please try again")
     }
   }
-
-  const getWeather = async () => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API}`)
-    const data = await response.json()
-    console.log(data)
-  }
-
-  const { data, isLoading, errorMessage } = useOpenWeather({
-    key: process.env.REACT_APP_WEATHER_API,
-    lat: lat.toString(),
-    lon: lon.toString(),
-    lang: 'en',
-    unit: 'imperial'
-  })
 
   useEffect(() => {
     const timer = setInterval(()=>setDate(new Date()), 1000)
@@ -48,39 +60,33 @@ function App() {
       clearInterval(timer)
     }
   })
+  
 
   return (
     <div className="App">
-      <Card>
+      <Card className="zipCard">
         <p>{msg}</p>
         <Form>
           <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm="2">Zip Code or City, State</Form.Label>
-            <Col sm="10">
+            <Form.Label column sm="2" className="label">Zip Code or City, State</Form.Label>
+            <Col>
               <Form.Control 
                 type="text" 
                 placeholder="12345" 
+                className="input"
                 onChange={(e) => setZip(e.target.value)}/>
             </Col>
-            <Button 
+          </Form.Group>
+          <Button 
               variant="light" 
               type="submit"
               onClick={getLonLat}>
               Submit
             </Button>
-          </Form.Group>
         </Form>
-        <p> Date : {date.toLocaleDateString()} | Time: {date.toLocaleTimeString()}</p>
+        <h5 className="dateTime">{date.toLocaleDateString()} | {date.toLocaleTimeString()}</h5>
       </Card>
-      <ReactWeather
-        isLoading={isLoading}
-        errorMessage={errorMessage}
-        data={data}
-        lang="en"
-        locationLabel="Lexington, KY"
-        unitsLabels={{ temperature: 'F', windSpeed: 'mph' }}
-        showForecast
-      />
+      {city !== "" ? <GetWeather lat={lat} lon={lon} city={city} /> : null}
     </div>
   )
 }
